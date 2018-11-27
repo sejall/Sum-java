ce1bb2cdb17e679ec1f91db641ec799be38ca5e1
sejall <33982474+sejall@users.noreply.github.com>
2018-11-27 19:59:30 +0530

    Add files via upload

---
+++ sumjava.java
@@ -0,0 +1,23 @@
+/**
+ * @(#)sumjava.java
+ *
+ *
+ * @author 
+ * @version 1.00 2018/2/7
+ */
+
+public class sumjava {
+        
+    /**
+     * Creates a new instance of <code>sumjava</code>.
+     */
+    public sumjava() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
